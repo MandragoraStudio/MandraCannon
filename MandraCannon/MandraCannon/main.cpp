b9f2ceba9ab0546d32123a6618d63dde42ed0048
unknown <Cinta@.(none)>
2010-12-20 21:03:44 +0100

    Algunas pruebas de imagenes .png Signed-off-by: unknown <Cinta@.(none)>

--- MandraCannon/MandraCannon/main.cpp
+++ MandraCannon/MandraCannon/main.cpp
@@ -2,12 +2,19 @@
 //
 
 #include "stdafx.h"
-#include "SDL.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#include "SDL_mixer.h"
+
 
 
 int main(int argc, char* argv[])
 {
-	SDL_Surface *screen;
+	SDL_Surface *screen, *image, *rot;
+	SDL_Rect dest, rrot;
 	SDL_Event event;
 	int done = 0;
 
@@ -16,6 +23,28 @@ int main(int argc, char* argv[])
 	SDL_Init(SDL_INIT_VIDEO);
 
 	screen = SDL_SetVideoMode(640,480,24,SDL_HWSURFACE);
+
+	printf("Se carga la imagen\n");
+	image = IMG_Load("pra.png");
+
+	dest.x = 0;
+	dest.y = 0;
+	dest.w = image->w;
+	dest.h = image->h;
+
+	rot = IMG_Load("rot.png");
+
+	rrot.x = 0;
+	rrot.y = 0;
+	rrot.w = rot->w;
+	rrot.h = rot->h;
+
+	SDL_BlitSurface(rot, NULL, screen, &rrot);
+	SDL_BlitSurface(image, NULL, screen, &dest);
+	SDL_Flip(screen);
+
+	SDL_FreeSurface(image);
+	SDL_FreeSurface(rot);
 
 	while(done==0){
 		while (SDL_PollEvent(&event)){

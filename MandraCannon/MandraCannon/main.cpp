2d9c070e72d109be9994809d132a06fb55e8253e
unknown <REMIX@.(none)>
2010-12-23 03:15:22 +0100

    Probando movimiento por teclado, sin exito Cambiado lo de cerrar el programa SDL por el botón ESCAPE
    Signed-off-by: unknown <REMIX@.(none)>

--- MandraCannon/MandraCannon/main.cpp
+++ MandraCannon/MandraCannon/main.cpp
@@ -10,15 +10,15 @@
 #include "SDL_rotozoom.h"
 #include "SSM.h"
 
-//probando cambios como comentario
-
 int main(int argc, char* argv[])
 {
 	SDL_Surface *screen, *image, *rot;
 	SDL_Rect dest, rrot;
 	SDL_Event event;
 	int done = 0;
+	int i = 1;
 	SSM pantalla;
+	Uint8 *keys;
 
 	atexit(SDL_Quit);
 
@@ -27,41 +27,58 @@ int main(int argc, char* argv[])
 
 	//pantalla.enabledFullScreen();
 	pantalla.setResolution(PANORAMICSMALL);
-	pantalla.setWindowsTitle("Amono señoresss");
+	pantalla.setWindowsTitle("MandraCannon");
 	pantalla.setIcon("recursos/logoventana/logoventana.png");
 	screen = pantalla.getSurface();
 	
 	printf("%f", pantalla.getPercent());
+	
 	image = IMG_Load("recursos/pruebas/pra.png");
 
-	dest.x = 200;
+	dest.x = 100;
 	dest.y = 200;
 	dest.w = image->w;
 	dest.h = image->h;
-
+	
 	rot = IMG_Load("recursos/pruebas/rot.png");
 
-	rrot.x = 0;
-	rrot.y = 0;
+	rrot.x = 10;
+	rrot.y = 10;
 	rrot.w = rot->w;
 	rrot.h = rot->h;
 
-	image = rotozoomSurface(image, 115, 4.5,  1);
-
-
+	image = rotozoomSurface(image, 135, 3.2, 1);
+	
 	SDL_BlitSurface(rot, NULL, screen, &rrot);
 	SDL_BlitSurface(image, NULL, screen, &dest);
+	
 	SDL_Flip(screen);
 
-	SDL_FreeSurface(image);
-	SDL_FreeSurface(rot);
+	keys=SDL_GetKeyState(NULL);
+	
+	if(keys[SDLK_w] == 1){
+		rrot.y=rrot.y-(5);
+	}
+	if(keys[SDLK_s] == 1){
+		rrot.y=rrot.y+(5);
+	}
+	if(keys[SDLK_d] == 1){
+		rrot.x=rrot.x+(5);
+	}
+	if(keys[SDLK_a] == 1){
+		rrot.x=rrot.x-(5);
+	}
 
 	while(done==0){
 		while (SDL_PollEvent(&event)){
-			if (event.type == SDL_KEYDOWN){
+			if (keys[SDLK_ESCAPE] == 1){
 				done = 1;
 			}
 		}
 	}
-	return 1;
+
+	SDL_FreeSurface(image);
+	SDL_FreeSurface(rot);
+	
+	return 0;
 }

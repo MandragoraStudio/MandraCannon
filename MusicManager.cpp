97c7c970fc49e265caef962b7a15ce96db8f37be
Spartanvs <amadorolivaresjuanamnuel@gmail.com>
2011-01-02 21:06:25 +0100

    Signed-off-by: Spartanvs <amadorolivaresjuanamnuel@gmail.com>

--- MusicManager.cpp
+++ MusicManager.cpp
@@ -6,13 +6,65 @@ Created by: Juan Manuel Amador Olivares
 
 #include "StdAfx.h"
 #include "MusicManager.h"
+#include "SDL_mixer.h"
 
+/*** Construtor ***/
+// Inicializa las variables
+MusicManager::MusicManager(void){}
 
-MusicManager::MusicManager(void)
-{
+/*** Deconstructor ***/
+// Destruye la memoria del objeto
+MusicManager::~MusicManager(void){}
+
+/*** loadMusic ***/
+// Carga una musica en la superficie
+int MusicManager::loadMusic(const char path[]){
+	if ((music = Mix_LoadMUS(path)) == NULL){			// Si no se puede cargar
+		return -1;										// sale y devuelve -1
+	}
+	return 0;
 }
 
+/*** setVolume ***/
+// Cambia el volumen de la musica (de 0 a 128)
+void MusicManager::setVolume(int volume){
+	Mix_VolumeMusic(volume);
+}
 
-MusicManager::~MusicManager(void)
-{
+/*** playChunk ***/
+// Reproduce el sonido tantas veces como se pase como argumento
+// Para que el sonido sea reproducido indefinidamente debe pasarse el argumento REPEAT
+// Para reproducirlo una vez debe pasarse el valor ONETIME
+void MusicManager::playMusic(int loops){
+	Mix_PlayMusic(music,loops);
+}
+
+/*** fadeInMusic ***/
+// Reproduce el sonido tantas veces como se pase como argumento con un efecto de volumen ascendente
+void MusicManager::fadeInMusic(int loops, int ms){
+	Mix_FadeInMusic(music,loops,ms);
+}
+
+/*** pause ***/
+// Pausa la reproduccion del chunk
+void MusicManager::pause(){
+	Mix_PauseMusic();
+}
+
+/*** resume ***/
+// Reanuda la reproduccion del chunk
+void MusicManager::resume(){
+	Mix_ResumeMusic();
+}
+
+/*** halt ***/
+// Para la reproduccion del chunk
+void MusicManager::halt(){
+	Mix_HaltMusic();
+}
+
+/*** fadeOut ***/
+// Para la reproduccion del chunk con un efecto de volumen ascendente
+void MusicManager::fadeOut(int ms){
+	Mix_FadeOutMusic(ms);
 }
--- MusicManager.h
+++ MusicManager.h
@@ -8,13 +8,25 @@ Created by: Juan Manuel Amador Olivares
 #include <SDL.h>
 #include "SDL_mixer.h"
 
+// Definiciones que describen la cantidad de veces que se reproducira una musica
+#define REPEAT -1
+#define ONETIME 0
+
 class MusicManager
 {
 protected:
-	Mix_Music *music;
+	Mix_Music *music;						// Estrutura que guardara la musica
+
 public:
 	MusicManager(void);						// Constructor, inicializa los atributos
 	~MusicManager(void);					// Deconstrutor
-	int loadMusic(const char path[]);			// Carga la musica en la superficie
+	int loadMusic(const char path[]);		// Carga la musica en la superficie
+	void setVolume(int volume);				// Cambia el volumen del de la musica (de 0 a 128)
+	void playMusic(int loops);				// Reproduce el sonido tantas veces como se pase como argumento
+	void fadeInMusic(int loops, int ms);	// Reproduce el sonido tantas veces como se pase como argumento con un efecto de volumen ascendente
+	void pause();							// Pausa la reproduccion de la musica
+	void resume();							// Reanuda la reproduccion del musica
+	void halt();							// Para la reproduccion de la musica
+	void fadeOut(int ms);					// Para la reproduccion de la musica con un efecto de volumen ascendente
 };
 
